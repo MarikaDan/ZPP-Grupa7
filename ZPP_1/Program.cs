6437b99bb1af0d730a605f015c0d5d1f2f456955
MarikaDan <126700019+MarikaDan@users.noreply.github.com>
2024-02-28 16:19:08 +0100

    MD:Dodano przykład
    Prosty przykład zamiany tekstu w pliku .docx za pomocą biblioteki npoi

--- ZPP_1/Program.cs
+++ ZPP_1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ZPP_1
+﻿using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ZPP_1
 {
     internal class Program
     {
+        static List<string> placeHolderDictionary = new List<string>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            placeHolderDictionary.Add("<imie>");
+            placeHolderDictionary.Add("<data>");
+
+            var tmp = @"D:\PLIKI\STUDIA\SEM6\ZPP\Projekt1\Template1.docx";
+
+            using (var rs = File.OpenRead(tmp))
+            {
+                var generateFile = @"D:\PLIKI\STUDIA\SEM6\ZPP\Projekt1\output1.docx";
+                using (var doc = new XWPFDocument(rs))
+                {
+                    foreach (var para in doc.Paragraphs)
+                    {
+                        foreach (var placeholder in placeHolderDictionary)
+                        {
+                            if (para.ParagraphText.Contains(placeholder))
+                            {
+                                para.ReplaceText(placeholder, "przykładowy tekst");
+                            }
+                        }
+                    }
+                    using (var ws = File.Create(generateFile))
+                    {
+                        doc.Write(ws);
+                    }
+                }
+
+            }
         }
     }
 }
