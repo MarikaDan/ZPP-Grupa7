d1c00d9ddf61f409b965dc77c8f7817129aed600
MarikaDan <126700019+MarikaDan@users.noreply.github.com>
2024-03-05 14:39:49 +0100

    Rozszerzono przykład
    Wprowadzono konwertowanie powstałego dokumntu .docx na .pdf

--- ZPP_1/Program.cs
+++ ZPP_1/Program.cs
@@ -1,7 +1,12 @@
 ﻿using NPOI.XWPF.UserModel;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.IO.Compression;
+using GrapeCity.Documents.Word;
+using System.Drawing;
+using GrapeCity.Documents.Word.Layout;
+
+
 namespace ZPP_1
 {
     internal class Program
@@ -15,9 +20,11 @@ namespace ZPP_1
 
             var tmp = @"D:\PLIKI\STUDIA\SEM6\ZPP\Projekt1\Template1.docx";
 
+
             using (var rs = File.OpenRead(tmp))
             {
                 var generateFile = @"D:\PLIKI\STUDIA\SEM6\ZPP\Projekt1\output1.docx";
+
                 using (var doc = new XWPFDocument(rs))
                 {
                     foreach (var para in doc.Paragraphs)
@@ -33,10 +40,32 @@ namespace ZPP_1
                     using (var ws = File.Create(generateFile))
                     {
                         doc.Write(ws);
+
+
                     }
+
                 }
 
             }
+
+            var wordDoc = new GcWordDocument();
+            var path = Path.Combine(@"D:\PLIKI\STUDIA\SEM6\ZPP\Projekt1\output1.docx");
+            wordDoc.Load(path);
+            using (var layout = new GcWordLayout(wordDoc))
+            {
+                PdfOutputSettings pdfOutput = new PdfOutputSettings();
+                pdfOutput.CompressionLevel = CompressionLevel.Fastest;
+                pdfOutput.ConformanceLevel = GrapeCity.Documents.Pdf.PdfAConformanceLevel.PdfA1a;
+                pdfOutput.BackColor = Color.Transparent;
+                pdfOutput.DocumentInfo = new GrapeCity.Documents.Pdf.DocumentInfo()
+                {
+                    Author = "Test",
+                    Title = "TestPDF",
+                    CreationDate = new GrapeCity.Documents.Pdf.PdfDateTime(DateTime.Now)
+                };
+                layout.SaveAsPdf(@"D:\PLIKI\STUDIA\SEM6\ZPP\Projekt1\output1.pdf", null, pdfOutput);
+            }
+
         }
     }
 }
